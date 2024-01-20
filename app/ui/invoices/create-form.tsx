355dd12e63ca752920ce59b0f407f12fa275c30b
Jaweki <werukioni@gmail.com>
2024-01-20 23:35:16 +0300

    added new route to create invoices; /app/dashboard/invoices/create. more functionality to be added soon such as react server actions, that will trigger creating new invoice

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -8,7 +8,9 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 
-export default function Form({ customers }: { customers: CustomerField[] }) {
+export default function Form({
+  customers,
+}: Readonly<{ customers: CustomerField[] }>) {
   return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
