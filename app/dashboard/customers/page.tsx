1678a4a5afc2debc6d4c7e8bc916d04de020fa2d
Jaweki <werukioni@gmail.com>
2024-01-30 14:26:40 +0300

    complete build of how to create a next.js app as guided by the next.js documentation. Date: January 30, 2024, by Jack Weru Kioni

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers Page',
+};
+
 const page = () => {
   return <p>Customers page</p>;
 };
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 // add searchParams as props for server-side component...
 // Anotated Page.tsx props to be readonly (Typescript)...
